a48afbf1a401680bdde592ddb89b56f4d99deaa5
pate1796 <harshd.patel@mail.utoronto.ca>
2022-11-25 22:59:03 -0500

    Added changes from Story1.5 branch

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -13,6 +13,7 @@ public class ClientHandler implements Runnable {
     private BufferedWriter writer;
     private String name;
     private Server server;
+    private Boolean eliminated;
 
     public ClientHandler(Socket socket, Server server) {
         try {
@@ -21,6 +22,7 @@ public class ClientHandler implements Runnable {
             this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.name = reader.readLine();
             this.server = server;
+            this.eliminated = false;
             sendMessageToServer("SERVER: " + this.name + " has joined");
         }
         //Disconnect client if there is an error connecting to the server
@@ -46,8 +48,45 @@ public class ClientHandler implements Runnable {
     }
 
     public void sendMessageToServer(String message) {this.server.readInput(message);}
+    public void sendMessageToClient(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        }
+        catch (IOException e) {closeEverything(this.socket, this.reader, this.writer);}
+    }
 
+    //called by Client
     public void removeClient() {sendMessageToServer(name + "| has left the game");}
+
+    public void eliminateClient() {
+        sendMessageToServer(name + "| was eliminated");
+        this.eliminated = true;
+        this.spectate();
+    }
+
+    public void spectate() {
+        while (!this.eliminated) {
+            sendMessageToServer(name + "| stats request");
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void statsToServer(String message) {
+        sendMessageToServer(message);
+    }
+
+
+
+    //called by Server
+    public void statsToClient(String message) {
+        sendMessageToClient(message);
+    }
 
     public void closeEverything(Socket socket, BufferedReader reader, BufferedWriter writer) {
         removeClient();
